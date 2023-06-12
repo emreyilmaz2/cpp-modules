84539a7504513e40da108d7b3e9a843a114e23c0
emreyimaz2 <emreeyilmaz567@gmail.com>
2023-06-12 23:01:40 +0300

    complete ex02

--- module5/ex02/PresidentialPardonForm.cpp
+++ module5/ex02/PresidentialPardonForm.cpp
@@ -1,1 +1,24 @@
 #include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+
+PresindentalPardonForm::PresindentalPardonForm(const std::string newTarget) : target(newTarget){
+    // std::cout << "PresidentalPardonForm default constructor called" << std::endl;
+}
+PresindentalPardonForm::PresindentalPardonForm(const PresindentalPardonForm& preCopy) : target(preCopy.getName()){
+    // std::cout << "PresidentalPardonForm copy constructor called" << std::endl;
+}
+PresindentalPardonForm& PresindentalPardonForm::operator=(const PresindentalPardonForm& preCopy){
+    AForm::operator=(preCopy);
+    // std::cout << "PresidentalPardonForm copy assigmnet operator called" << std::endl;   
+}
+PresindentalPardonForm::~PresindentalPardonForm(){
+    // std::cout << "PresidentalPardonForm destructor called" << std::endl;   
+}
+
+void PresindentalPardonForm::execute(const Bureaucrat& executor) const{
+    if(!getSign())
+        throw AForm::NotEnoughToSign();
+    else if(executor.getGrade() > getGradeToExecute())
+        throw  AForm::NotEnoughToExecute();
+    std::cout << getName() << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
--- module5/ex02/PresidentialPardonForm.hpp
+++ module5/ex02/PresidentialPardonForm.hpp
@@ -6,6 +6,13 @@
 class PresindentalPardonForm : public AForm{
 private:
     std::string target;
+public:
+    PresindentalPardonForm(const std::string newTarget);
+    PresindentalPardonForm(const PresindentalPardonForm& preCopy);
+    PresindentalPardonForm& operator=(const PresindentalPardonForm& preCopy);
+    ~PresindentalPardonForm();
+
+    void execute(const Bureaucrat& executor) const;
 };
 
 #endif
