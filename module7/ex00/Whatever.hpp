26a3575d5691203ce3b703f02631ce01a756b029
emreyimaz2 <emreeyilmaz567@gmail.com>
2023-07-21 23:38:20 +0300

    dealing process w 7 is done

--- module7/ex00/Whatever.hpp
+++ module7/ex00/Whatever.hpp
@@ -13,19 +13,12 @@ template<typename t_swap> void swap(t_swap &a, t_swap &b){
 
 template<typename t_min> t_min min(t_min &a, t_min &b)
 {
-    if(a == b)
-        return b;
-    else if(a < b)
-        return a;
-    return b;
+    return((a < b) ? a : b);
 }
 
-template<typename t_max> t_max max(t_max &a, t_max &b){
-    if(a == b)
-        return b;
-    else if(a > b)
-        return a;
-    return b;
+template<typename t_max> t_max max(t_max &a, t_max &b)
+{
+    return((a > b) ? a : b);
 }
 
 
